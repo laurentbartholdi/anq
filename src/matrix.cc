b85e6ebdb92b551b7e17fd09d5d4daca124db135
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2018-03-02 16:39:09 +0100

    Optimized memory management in AddRow

--- src/matrix.cc
+++ src/matrix.cc
@@ -6,36 +6,43 @@
 */
 
 #include "lienq.h"
+#include <algorithm>
 
 static unsigned NrRows;
 static gpvec *Matrix;
 
 void InitMatrix(void) {
-  Matrix = (gpvec *) malloc(NrCenGens * sizeof(gpvec));
+  Matrix = (gpvec *) malloc((NrCenGens+1) * sizeof(gpvec));
   if (Matrix == NULL)
     abortprintf(2, "InitMatrix: malloc(Matrix) failed");
+
+  Matrix[0] = NewVec(NrTotalGens); // scratch row
 
   NrRows = 0;
 }
 
 void FreeMatrix(void) {
-  for (unsigned i = 0; i < NrRows; i++)
+  for (unsigned i = 0; i <= NrRows; i++)
     FreeVec(Matrix[i], NrTotalGens);
   free(Matrix);
 }
 
+inline void SwapRows(unsigned i, unsigned j) {
+  gpvec v = Matrix[i];
+  Matrix[i] = Matrix[j];
+  Matrix[j] = v;
+}
+
 /* v -= v[w->g]/w->c * w */
-void ReduceRow(gpvec &v, gpvec &w) {
-  for (gpvec p = v; p->g != EOW; p++)
+void ReduceRow(unsigned row, gpvec &w) {
+  for (gpvec p = Matrix[row]; p->g != EOW; p++)
     if (p->g == w->g) {
       coeff q;
       coeff_init (q);
       coeff_fdiv_q(q, p->c, w->c);
       if (coeff_nz_p(q)) {
-	gpvec temp = FreshVec();
-	Diff(temp, v, q, w);
-	Copy(v, temp);
-	PopVec();
+	Diff(Matrix[NrRows], Matrix[row], q, w);
+	SwapRows(row, NrRows);
 	coeff_clear(q);
 	break;
       }
@@ -47,7 +54,7 @@ void HermiteNormalForm(gpvec **rels, unsigned *numrels) {
   /* reduce all the head columns, to achieve Hermite normal form. */
   for (unsigned i = 1; i < NrRows; i++)
     for (unsigned j = 0; j < i; j++)
-      ReduceRow(Matrix[j], Matrix[i]);
+      ReduceRow(j, Matrix[i]);
 
   *rels = Matrix;
   *numrels = NrRows;
@@ -59,14 +66,17 @@ void HermiteNormalForm(gpvec **rels, unsigned *numrels) {
 void PartialHermite(unsigned row) {
   return;
   for (unsigned j = 0; j < row; j++)
-    ReduceRow(Matrix[j], Matrix[row]);
+    ReduceRow(j, Matrix[row]);
   return;
   for (unsigned j = row+1; j < NrRows; j++)
-    ReduceRow(Matrix[row], Matrix[j]);
+    ReduceRow(row, Matrix[j]);
   return;
 }
 #endif
 
+/* adds a row to the current matrix, and returns true if the matrix was
+   modified.
+   the vector cv may be modified during the process */
 bool AddRow(gpvec cv) {
   if (cv->g == EOW) // easy case: trivial relation, change nothing
     return false;
@@ -76,70 +86,64 @@ bool AddRow(gpvec cv) {
 
   bool ChangedMatrix = false;
 
-
-  gpvec p = FreshVec();
-  Copy(p, cv);
+  gpvec p[2] = { cv, FreshVec() };
+  bool parity = false;
   
-  for (unsigned row = 0; p->g != EOW; row++) {
-    if (row >= NrRows || Matrix[row]->g > p->g) {
+  for (unsigned row = 0; p[parity]->g != EOW; row++) {
+    if (row >= NrRows || Matrix[row]->g > p[parity]->g) {
       /* Insert v in Matrix at position row */
       ChangedMatrix = true;
 
       coeff unit, annihilator;
       coeff_init(unit);
       coeff_init(annihilator);
-      coeff_unit_annihilator(unit, annihilator, p->c);
+      coeff_unit_annihilator(unit, annihilator, p[parity]->c);
 
-      for (unsigned i = NrRows++; i > row; i--)
+      for (unsigned i = ++NrRows; i > row; i--)
 	Matrix[i] = Matrix[i-1];
       Matrix[row] = NewVec(NrTotalGens);
-      Prod(Matrix[row], unit, p);
+      Prod(Matrix[row], unit, p[parity]);
+
+      for (unsigned j = row+1; j < NrRows; j++)
+	ReduceRow(row, Matrix[j]);
 
       if (Debug >= 3) {
 	printf("ADD ROW %d: ",row); PrintVec(Matrix[row]); printf("\n");
       }
 
-      Prod(p, annihilator, p);
+      Prod(p[parity], annihilator, p[parity]);
       coeff_clear(unit);
       coeff_clear(annihilator);
-    } else if (Matrix[row]->g == p->g) { /* two rows with same pivot. Merge them */
+    } else if (Matrix[row]->g == p[parity]->g) { /* two rows with same pivot. Merge them */
       coeff a, b, c, d;
       coeff_init(a);
       coeff_init(b);
       coeff_init(c);
       coeff_init(d);
-      coeff_gcdext(d, a, b, p->c, Matrix[row]->c); /* d = a*v[head]+b*Matrix[row][head] */
+      coeff_gcdext(d, a, b, p[parity]->c, Matrix[row]->c); /* d = a*v[head]+b*Matrix[row][head] */
       if (!coeff_cmp(d, Matrix[row]->c)) { /* likely case: Matrix[row][head]=d, b=1, a=0 */
-	coeff_divexact(d,p->c,d);
-	gpvec temp = FreshVec();
-	Diff(temp, p, d, Matrix[row]);
-#if 0
-	PopVec(p);
-#else
-	Copy(p, temp);
-	PopVec();
+	coeff_divexact(d, p[parity]->c, d);
+	Diff(p[!parity], p[parity], d, Matrix[row]);
+	parity ^= 1;	
+#ifdef COEFF_IS_MPZ // check coefficient explosion
+      if (Debug >= 1) {
+	long maxsize = 0;
+	for (gpvec q = p[parity]; q->g != EOW; q++)
+	  maxsize = std::max(maxsize, labs(q->c->_mp_size));
+	printf("CHANGE p: max coeff size %ld\n", maxsize);
+      }
 #endif
 	
       } else {
 	ChangedMatrix = true;
-	coeff_divexact(c,p->c,d);
-	coeff_divexact(d,Matrix[row]->c,d);
-	gpvec newp = FreshVec();
-	gpvec newrow = FreshVec();
-	Sum(newrow, a, p, b, Matrix[row]);
-	Diff(newp, c, Matrix[row], d, p);
-#if 0
-	PopVec(Matrix[row]); // pops newrow. Works only if Matrix allocated on stack
-#else	
-	Copy(Matrix[row], newrow);
-	PopVec(); // pops newrow
-#endif
-#if 0
-	PopVec(p);
-#else
-	Copy(p, newp);
-	PopVec();
-#endif
+
+	coeff_divexact(c, p[parity]->c, d);
+	coeff_divexact(d, Matrix[row]->c, d);
+	Sum(Matrix[NrRows], a, p[parity], b, Matrix[row]);
+	Diff(p[!parity], c, Matrix[row], d, p[parity]);
+	SwapRows(row, NrRows);
+	parity ^= 1;
+
 	if (Debug >= 3) {
 	  printf("CHANGE ROW %d: ",row); PrintVec(Matrix[row]); printf("\n");
 	}
@@ -151,7 +155,7 @@ bool AddRow(gpvec cv) {
     }
   }
 
-  PopVec(); // free p
+  PopVec(); // free p[true]
   
   return ChangedMatrix;
 }
